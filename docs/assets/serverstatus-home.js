87f1c9ae7955f5803203bb5559a369a094104b6d
AmizRm <amirrezahp86@gmail.com>
2024-06-27 18:43:12 +0330

    sv version

--- docs/assets/serverstatus-home.js
+++ docs/assets/serverstatus-home.js
@@ -3,7 +3,7 @@
 {% include serverstatus.js %}
 .then(data => {
     if (data.online == true) {
-        document.getElementById("serverVer").innerHTML = `Minecraft ${data.version} Server`;
+        document.getElementById("serverVer").innerHTML = `Minecraft ${data.version.name_raw} Server`;
 
         // siteNav dependent
         {% if site.siteNav %}
